03552de3efee624a1785dcf1eb708e5848bd237d
Tony Ma <tonymazn@gmail.com>
2018-11-16 20:23:00 -0700

    sanitize text

--- application/textsummarize.py
+++ application/textsummarize.py
@@ -1,6 +1,18 @@
 from summa.summarizer import summarize
+from string import punctuation
 
 
 def getsummary(text):
+    text = sanitize(text)
     summary = summarize(text)
     return summary
+
+def sanitize(text):
+    removechartlist = {
+        ord('\f') : ' ',
+        ord('\t') : ' ',
+        ord('\n') : ' ',
+        ord('\r') : None
+    }
+    return text.translate(removechartlist)
+
