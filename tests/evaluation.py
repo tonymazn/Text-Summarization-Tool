698560666e6220418beafe27b9119a8ca4b5f5bb
Tony Ma <tonymazn@gmail.com>
2018-11-18 09:56:05 -0700

    First Run ROUGE

--- tests/evaluation.py
+++ tests/evaluation.py
@@ -3,12 +3,45 @@
 from pyrouge import Rouge155
 
 
-def testresult():
+def lexrank_testresult():
     r = Rouge155()
-    r.system_dir = 'C:/CS410/Data/duc2004_2/system'
+    r.system_dir = 'C:/CS410/Data/duc2004_2/system_lexrank'
     r.model_dir = 'C:/CS410/Data/duc2004_2/models'
-    r.system_filename_pattern = 'D(\d+)T.TextRank.txt'
-    r.model_filename_pattern = 'D#ID#.M.100.T.[A-D]'
+    r.system_filename_pattern = 'D(\d+)T.txt'
+    r.model_filename_pattern = 'D#ID#.M.100.T.[A-Z]'
+
+    output = r.convert_and_evaluate()
+    print(output)
+    output_dict = r.output_to_dict(output)
+
+def luhn_testresult():
+    r = Rouge155()
+    r.system_dir = 'C:/CS410/Data/duc2004_2/system_luhn'
+    r.model_dir = 'C:/CS410/Data/duc2004_2/models'
+    r.system_filename_pattern = 'D(\d+)T.txt'
+    r.model_filename_pattern = 'D#ID#.M.100.T.[A-Z]'
+
+    output = r.convert_and_evaluate()
+    print(output)
+    output_dict = r.output_to_dict(output)
+
+def las_testresult():
+    r = Rouge155()
+    r.system_dir = 'C:/CS410/Data/duc2004_2/system_las'
+    r.model_dir = 'C:/CS410/Data/duc2004_2/models'
+    r.system_filename_pattern = 'D(\d+)T.txt'
+    r.model_filename_pattern = 'D#ID#.M.100.T.[A-Z]'
+
+    output = r.convert_and_evaluate()
+    print(output)
+    output_dict = r.output_to_dict(output)
+
+def textrank_testresult():
+    r = Rouge155()
+    r.system_dir = 'C:/CS410/Data/duc2004_2/system_textrank'
+    r.model_dir = 'C:/CS410/Data/duc2004_2/models'
+    r.system_filename_pattern = 'D(\d+)T.txt'
+    r.model_filename_pattern = 'D#ID#.M.100.T.[A-Z]'
 
     output = r.convert_and_evaluate()
     print(output)
@@ -16,4 +49,4 @@ def testresult():
 
 
 if __name__ == '__main__':
-    testresult()
+    textrank_testresult()
--- tests/runsystem.py
+++ tests/runsystem.py
@@ -6,31 +6,59 @@ from core.summarizer import *
 from core.algorithms import algorithms
 from .utils import *
 
-
 class runsystem(unittest.TestCase):
-    def test_create_file(self):
-        #create_file("C:\\CS410\\Data\\duc2004_2\\system", "test.txt", "Woops! I have deleted the content!")
-        self.assertEqual(1+1, 2)
+    def test_generate_lexrank_summary(self):
+        generate_summary(algorithms.LexRank, "system_lexrank")
 
-    def test_generate_summary(self):
-       test_data_path = "C:\\CS410\\Data\\duc2004_2\\testdata\\"
-       cluster_folder = get_dir_list(test_data_path)
-       for dir in cluster_folder:
-           cluster_whole_path = os.path.join(os.path.dirname(test_data_path), dir)
-           print(cluster_whole_path)
-           dirs = get_file_list(cluster_whole_path)
-           summary_result = ""
-           for file in dirs:
-              print("read file", end='')
-              print(file)
-              context = read_file(cluster_whole_path, file)
-              #print(context)
-              summarizer = get_summary_result(context, 1, algorithms.TextRank )
-              #print(summarizer)
-              summary_result +=convert_to_text(summarizer)
-              summary_result +=get_newline()
-              print(summary_result)
-              create_file("C:\\CS410\\Data\\duc2004_2\\system", dir.upper() + ".TextRank.txt", summary_result)
+    def test_generate_luhn_summary(self):
+        generate_summary(algorithms.Luhn, "system_luhn")
+
+    def test_generate_las_summary(self):
+        generate_summary(algorithms.LAS, "system_las")
+
+#    def test_generate_lexrank_summary():
+#       test_data_path = "C:\\CS410\\Data\\duc2004_2\\testdata\\"
+#       cluster_folder = get_dir_list(test_data_path)
+#       for dir in cluster_folder:
+#           cluster_whole_path = os.path.join(os.path.dirname(test_data_path), dir)
+#           print(cluster_whole_path)
+#           dirs = get_file_list(cluster_whole_path)
+#           summary_result = ""
+#           for file in dirs:
+#              print("read file", end='')
+#              print(file)
+#              context = read_file(cluster_whole_path, file)
+#              #print(context)
+#
+#              #change the algorithms to gernate difference result, and create
+#              #files into system folder
+#              summarizer = get_summary_result(context, 1, algorithms.LexRank)
+#              #print(summarizer)
+#              summary_result +=convert_to_text(summarizer)
+#              summary_result +=get_newline()
+#              print(summary_result)
+#              create_file("C:\\CS410\\Data\\duc2004_2\\system_lexrank", dir.upper() + ".LexRank.txt", summary_result)
+    def test_generate_textrank_summary(self):
+        generate_summary(algorithms.TextRank, "system_textrank")
+
+
+def generate_summary(algorithm, system_folder_name):
+    test_data_path = "C:\\CS410\\Data\\duc2004_2\\testdata\\"
+    cluster_folder = get_dir_list(test_data_path)
+    for dir in cluster_folder:
+        cluster_whole_path = os.path.join(os.path.dirname(test_data_path), dir)
+        print(cluster_whole_path)
+        dirs = get_file_list(cluster_whole_path)
+        summary_result = ""
+        for file in dirs:
+            print("read file", end='')
+            print(file)
+            context = read_file(cluster_whole_path, file)
+            summarizer = get_summary_result(context, 1, algorithm)
+            summary_result +=convert_to_text(summarizer)
+            summary_result +=get_newline()
+            print(summary_result)
+        create_file("C:\\CS410\\Data\\duc2004_2\\" + system_folder_name, dir.upper() + ".txt", summary_result)
 
 if __name__ == '__main__':
     unittest.main()
